fc9937248eeee5a0c06a86b42a79ff32f8f272db
Jack Arendt <jack.arendt93@gmail.com>
2018-04-01 15:48:03 -0700

    Added css, and moved design into git.

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
+  """Entry point to website. Return home.html and populate it with latest data."""
   with open('html/home.html') as f:
     html = f.read()
     return storage.replace_bucket_name(html)
 
 @app.errorhandler(500)
 def server_error(e):
-    logging.exception('An error occurred during a request.')
-    return """
-    An internal error occurred: <pre>{}</pre>
-    See logs for full stacktrace.
-    """.format(e), 500
+  """Log the exception, and send back a custom error page."""
+  logging.exception('An error occurred during a request.')
+  return """
+  An internal error occurred: <pre>{}</pre>
+  See logs for full stacktrace.
+  """.format(e), 500
